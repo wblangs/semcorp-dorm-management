dfc6408962a3dc51652a089bbf8dea2b49bc7d0c
yiyaozhang2003 <zhangyiyao20030605@gmail.com>
2026-06-04 09:57:58 -0400

    fix depoly database bug

--- alembic/env.py
+++ alembic/env.py
@@ -7,7 +7,7 @@ from backend.core.config import settings
 from backend.models import Base
 
 config = context.config
-config.set_main_option("sqlalchemy.url", settings.database_url)
+config.set_main_option("sqlalchemy.url", settings.database_url.replace("%", "%%"))
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
